e4408e1b79375a5f3f7b52525b41b78a7b0dd93e
Flenryk <dev@flenryk.dev>
2025-10-14 22:31:07 -0400

    Update conf.lua

--- conf.lua
+++ conf.lua
@@ -9,7 +9,7 @@ function love.conf(t)
     t.audio.mixwithsystem = true       
 
     t.window.title = " Don't touch me "   
-    t.author = "Moises Perez" 
+    t.author = "flenryk" 
     t.window.icon = 'images/icon.png'
     t.window.width = 500            
     t.window.height = 700           
